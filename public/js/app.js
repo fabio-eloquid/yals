8e235227be14feea9e9c9a7394b5d88c9a5ad163
fabio.eloquid <fabio@eloquid.com>
2023-11-23 07:09:49 +0100

    wip

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,52 @@
-let auth0 = null;
+let auth0Client = null;
 
 const fetchAuthConfig = () => fetch("/auth_config.json");
 
 const configureClient = async () => {
   const response = await fetchAuthConfig();
   const config = await response.json();
+  console.log("domain "+config.domain);
+  console.log("clientId "+config.clientId);
+  console.log("audience "+config.audience);
 
-  auth0 = await createAuth0Client({
+  auth0Client = await createAuth0Client({
     domain: config.domain,
-    client_id: config.clientId
+    //clientId: config.clientId,
+    client_id: config.clientId,
+    authorizationParams: {
+      audience: config.audience   // NEW - add the audience value
+    }
   });
+   console.log('> auth0 obj', auth0Client);
+
+};
+
+const callApi = async () => {
+  try {
+
+    // Get the access token from the Auth0 client
+    const token = await auth0Client.getTokenSilently();
+
+    // Make the call to the API, setting the token
+    // in the Authorization header
+    const response = await fetch("/api/external", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    // Fetch the JSON result
+    const responseData = await response.json();
+
+    // Display the result in the output element
+    const responseElement = document.getElementById("api-call-result");
+
+    responseElement.innerText = JSON.stringify(responseData, {}, 2);
+
+} catch (e) {
+    // Display errors in the console
+    console.error(e);
+  }
 };
 
 window.onload = async () => {
@@ -18,33 +55,47 @@ window.onload = async () => {
   // NEW - update the UI state
   updateUI();
 
-  const isAuthenticated = await auth0.isAuthenticated();
+  const isAuthenticated = await auth0Client.isAuthenticated();
 
   if (isAuthenticated) {
     // show the gated content
     return;
   }
-
-  // NEW - check for the code and state parameters
+  // Check for the code and state parameters
   const query = window.location.search;
   if (query.includes("code=") && query.includes("state=")) {
+	  const params = new URLSearchParams(query);
+	  const code = params.get('code');
+	  const state = params.get('state');
 
-    // Process the login state
-    await auth0.handleRedirectCallback();
+	  if (code && state) {
+		// Display code and state on the screen
+		document.getElementById('ipt-access-token').textContent = `Code: ${code}`;
+		document.getElementById('ipt-state').textContent = `State: ${state}`;
+		console.log(code);
+		console.log(state);
 
-    updateUI();
 
-    // Use replaceState to redirect the user away and remove the querystring parameters
-    window.history.replaceState({}, document.title, "/");
+		// Process the login state
+		await auth0Client.handleRedirectCallback();
+
+		updateUI();
+
+		// Use replaceState to redirect the user away and remove the querystring parameters
+		window.history.replaceState({}, document.title, "/");
+	  }
   }
 };
 
 // NEW
 const updateUI = async () => {
-  const isAuthenticated = await auth0.isAuthenticated();
+  const isAuthenticated = await auth0Client.isAuthenticated();
 
   document.getElementById("btn-logout").disabled = !isAuthenticated;
   document.getElementById("btn-login").disabled = isAuthenticated;
+
+  // NEW - enable the button to call the API
+  document.getElementById("btn-call-api").disabled = !isAuthenticated;
   
   // NEW - add logic to show/hide gated content after authentication
   if (isAuthenticated) {
@@ -52,10 +103,10 @@ const updateUI = async () => {
 
     document.getElementById(
       "ipt-access-token"
-    ).innerHTML = await auth0.getTokenSilently();
+    ).innerHTML = await auth0Client.getTokenSilently();
 
     document.getElementById("ipt-user-profile").textContent = JSON.stringify(
-      await auth0.getUser()
+      await auth0Client.getUser()
     );
 
   } else {
@@ -64,14 +115,14 @@ const updateUI = async () => {
 };
 
 const login = async () => {
-  await auth0.loginWithRedirect( // authorizationParams:
+  await auth0Client.loginWithRedirect( // authorizationParams:
   {
     redirect_uri: window.location.origin
   });
 };
 
 const logout = () => {
-  auth0.logout( //  logoutParams:
+  auth0Client.logout( //  logoutParams:
   {
     returnTo: window.location.origin
   });
--- server.js
+++ server.js
@@ -1,19 +1,71 @@
 const express = require("express");
 const { join } = require("path");
 const app = express();
+const { auth } = require("express-oauth2-jwt-bearer");
+const authConfig = require("./auth_config.json");
+
+console.log("Auth Config:", authConfig);
 
 // Serve static assets from the /public folder
 app.use(express.static(join(__dirname, "public")));
+
+if (!authConfig.domain || !authConfig.audience) {
+  throw "Please make sure that auth_config.json is in place and populated";
+}
+
+// Middleware to log requests
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.path}`);
+  console.log("Headers:", req.headers);
+  next();
+});
+
+console.log("checkJwt middleware initialized with:", { 
+  audience: authConfig.audience, 
+  issuerBaseURL: `https://${authConfig.domain}` 
+});
+
+const checkJwt = auth({
+  audience: authConfig.audience,
+  issuerBaseURL: `https://${authConfig.domain}`,
+});
 
 // Endpoint to serve the configuration file
 app.get("/auth_config.json", (req, res) => {
   res.sendFile(join(__dirname, "auth_config.json"));
 });
 
+app.get("/api/external", (req, res, next) => {
+  console.log("Accessing /api/external");
+  return checkJwt(req, res, next);
+}, (req, res) => {
+  res.send({
+    msg: "Your access token was successfully validated!"
+  });
+});
+
 // Serve the index page for all other requests
 app.get("/*", (_, res) => {
+  console.log("Serving index.html");
   res.sendFile(join(__dirname, "index.html"));
 });
 
-// Listen on port 3000
-app.listen(3000, () => console.log("Application running on port 3000"));
+app.use(function(err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).send({ msg: "Invalid token" });
+  }
+
+  next(err, req, res);
+});
+
+// Error handling middleware
+app.use(function(err, req, res, next) {
+  console.error("Error caught in middleware:", err);
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).send({ msg: "Invalid token" });
+  }
+  next(err, req, res);
+});
+
+// Listen on port 4000
+app.listen(4000, () => console.log("Application running on port 4000"));
